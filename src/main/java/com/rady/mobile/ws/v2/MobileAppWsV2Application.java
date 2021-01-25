a11d48d9e1c059bbb8cc5ed9123f67346a02cc0e
Rady Ahmed <rady.ahmed999@gmail.com>
2021-01-26 00:11:40 +0200

    Initial commit

---
+++ src/main/java/com/rady/mobile/ws/v2/MobileAppWsV2Application.java
@@ -0,0 +1,13 @@
+package com.rady.mobile.ws.v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileAppWsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileAppWsV2Application.class, args);
+	}
+String s="radu";
+}
---
+++ src/test/java/com/rady/mobile/ws/v2/MobileAppWsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rady.mobile.ws.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileAppWsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
