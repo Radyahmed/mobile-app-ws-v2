826e55300673604f52c0b5420ed90e6c0afd686b
Rady Ahmed <rady.ahmed999@gmail.com>
2021-02-25 19:42:34 +0200

    GetUserDetails

--- src/main/java/com/rady/mobile/ws/v2/io/repositories/UserRepository.java
+++ src/main/java/com/rady/mobile/ws/v2/io/repositories/UserRepository.java
@@ -8,4 +8,7 @@ import com.rady.mobile.ws.v2.io.entity.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 	UserEntity findByEmail(String email);
+
+	UserEntity findByUserId(String id);
+
 }
