7b5be93c30467b489ce308fa05b9bfbdfca0af6f
Rady Ahmed <rady.ahmed999@gmail.com>
2021-02-27 23:14:56 +0200

    deployWebService

--- src/main/java/com/rady/mobile/ws/v2/MobileAppWsV2Application.java
+++ src/main/java/com/rady/mobile/ws/v2/MobileAppWsV2Application.java
@@ -2,13 +2,20 @@ package com.rady.mobile.ws.v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.rady.mobile.ws.v2.security.AppProperties;
 
 @SpringBootApplication
-public class MobileAppWsV2Application {
+public class MobileAppWsV2Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MobileAppWsV2Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsV2Application.class, args);
